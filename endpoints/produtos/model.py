6810a7eaee7f79f844d4f967a566da5f7427f439
Vitor Beck <toorugoo@gmail.com>
2022-12-15 17:06:07 -0300

    models and methods for produtos

--- endpoints/produtos/model.py
+++ endpoints/produtos/model.py
@@ -1,15 +1,18 @@
+from datetime import datetime
+
 from app import db
 
+
 class Produto(db.Model):
-    _tablename_ = 'tbl_produtos'
+    _tablename_ = "tbl_produtos"
 
-    id = db.Column(db.Integer, primary_key=True,nullable=False)
-    nome = db.Column(db.String(), nullable=False)
-    ativo = db.Column(db.Bool, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    nome = db.Column(db.String(), nullable=False, unique=True)
+    creation_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    ativo = db.Column(db.Boolean, nullable=False)
 
-    def __init__(self, id, nome, ativo):
+    def _init_(self, id, ativo):
         self.id = id
-        self.nome = nome
         self.ativo = ativo
 
     def _repr_(self):
---
+++ endpoints/produtos/resource.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+
+from flask_restful import fields
+from flask_restful import marshal
+from flask_restful import marshal_with
+from flask_restful import reqparse
+from flask_restful import Resource
+
+from .model import Produto
+from app import db
+
+produto_fields = {
+    "id": fields.Integer,
+    "nome": fields.String,
+}
+
+
+
+produto_post_parser = reqparse.RequestParser()
+produto_post_parser.add_argument(
+    "nome",
+    type=str,
+    required=True,
+    location=["json"],
+    help="O parâmetro nome é obrigatório",
+)
+
+class ProdutosResource(Resource):
+    def get(self, id=None):
+        if id:
+            prod = Produto.query.filter_by(id=id).first()
+            return marshal(prod, produto_fields)
+
+    @marshal_with(produto_fields)
+    def post(self):
+        args = produto_post_parser.parse_args()
+        args["ativo"] = True
+        prod = Produto(**args)
+
+        db.session.add(prod)
+        db.session.commit()
+
+        return prod
+
+
+        
